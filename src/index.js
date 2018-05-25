9d52f031dcdeef8f16477dbca9a05b77ddb2fd6e
github username <github email address>
2018-05-25 11:00:49 -0400

    Here it is

--- src/index.js
+++ src/index.js
@@ -1,7 +1,54 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const imageId = 1 //Enter your assigned imageId here
+  const imageId = 15 //Enter your assigned imageId here
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
   const likeURL = `https://randopic.herokuapp.com/likes/`
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+  let image = document.getElementById('image')
+  let name = document.getElementById('name')
+  let likes = document.getElementById('likes')
+  let comment = document.getElementById('comments')
+  const likeButton = document.getElementById('like_button')
+
+
+function incrementLikes(event) {
+  event.preventDefault()
+  debugger
+  likes.innerText += 1
+}
+
+likeButton.addEventListener('click', incrementLikes)
+
+function appendInfoToDom(objs) {
+  image.innerHTML += objs.url
+  name.innerHTML += objs.name
+  likes.innerHTML += objs.like_count
+  comment.innerHTML += `<li>${objs.comments[0].content}</li>`
+
+}
+
+
+function fetchMessages() {
+  fetch(imageURL)
+  .then(r=>r.json())
+  .then(objs => {
+    appendInfoToDom(objs)
+  })
+}
+
+function postLikesToDom() {
+  const config {
+    method: 'POST',
+    headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}
+    body: {JSON.stringify({})}
+
+
+  }
+
+
+
+}
+fetchMessages()
+
+
 
 })
