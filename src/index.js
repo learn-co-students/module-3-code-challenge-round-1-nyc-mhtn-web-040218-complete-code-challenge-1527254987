88f6da6951a8f64c61b8c7df7622d41b343d6a60
SuperJesseH <therealjhorwitz@gmail.com>
2018-05-25 10:58:22 -0400

    got delete function working

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const likeButton = document.getElementById('like_button')
   const commentForm = document.getElementById('comment_form')
+  const imageComments = document.getElementById('comments')
+
+  const commentStore = {}
 
 console.log('DOM LOADED')
 
@@ -27,13 +30,23 @@ console.log('DOM LOADED')
     const imageLikes = document.getElementById('likes')
     imageLikes.innerHTML = imageJson.like_count
 
-    const imageComments = document.getElementById('comments')
+    //const imageComments = document.getElementById('comments')
     for (const comment of imageJson.comments){
       const singleComment = document.createElement('li')
       singleComment.setAttribute('id',`comment-${comment.id}`)
-      singleComment.innerHTML = comment.content
+      singleComment.innerHTML = comment.content + " "
+      const deleteButton = document.createElement('button')
+      deleteButton.setAttribute('type','button')
+      deleteButton.innerHTML = "❌"
+      singleComment.append(deleteButton)
       imageComments.append(singleComment)
     }
+  }
+
+
+/// This is weird but works out
+  function storeNewComment(resp, newComment) {
+    newComment.setAttribute('id',`comment-${resp.id}`)
   }
 
 likeButton.addEventListener('click', (e)=>{
@@ -55,16 +68,28 @@ commentForm.addEventListener('submit', (e)=>{
   const imageComments = document.getElementById('comments')
   const newComment = document.createElement('li')
   newComment.innerHTML = e.srcElement[0].value
+  const deleteButton = document.createElement('button')
+  deleteButton.setAttribute('type','button')
+  deleteButton.innerHTML = "❌"
+  newComment.append(deleteButton)
   imageComments.append(newComment)
 
   fetch(commentsURL,{method:'POST',
    headers:{
   'Accept': 'application/json',
   'Content-Type': 'application/json'},
-   body: JSON.stringify({image_id:8, content:e.srcElement[0].value})})
-
+   body: JSON.stringify({image_id:8, content:e.srcElement[0].value})}).then(r=>r.json()).then(r=>storeNewComment(r, newComment))
 
   console.log('SUBMIT')
+})
+
+imageComments.addEventListener('click', (e)=>{
+  if(e.target.type === 'button'){
+    const selectedComment = document.getElementById(e.target.parentElement.id)
+    selectedComment.remove()
+
+    fetch(commentsURL + selectedComment.id.split('-')[1], {method:'DELETE'})
+  }
 })
 
 
